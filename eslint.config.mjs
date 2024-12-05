dd70d48944e58404198a07c7d3ad3f794034824c
Sana Khuram <sana.khuram.baig@gmail.com>
2024-12-05 15:16:04 +0100

    ADD ESLint

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+
+/** @type {import('eslint').Linter.Config} */
+export default {
+  ...pluginJs.configs.recommended,
+  languageOptions: {
+    globals: {
+      ...globals.browser,
+      module: true,
+      require: true,
+      process: true,
+      test: true,
+    },
+  },
+  rules: {
+    'no-unused-vars': ['error', { args: 'all' }], 
+  },
+};
